6cd0ccd75d952da3405d27c79abb76e4bba8e123
desh <serdesh77@mail.ru>
2019-04-08 12:55:41 +0300

    init

---
+++ migrations/m190408_094412_create_task_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%task}}`.
+ */
+class m190408_094412_create_task_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%task}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%task}}');
+    }
+}
